9201e7970d45390cb326df20d19bccdf8ba68d55
Dave Syer <dsyer@vmware.com>
2023-09-08 17:36:58 +0100

    Play with links

--- src/test/java/com/example/gateway/OpenApiValidationTests.java
+++ src/test/java/com/example/gateway/OpenApiValidationTests.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.OpenAPIResolver;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.parser.core.models.ParseOptions;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
@@ -21,6 +22,14 @@ public class OpenApiValidationTests {
 		OpenAPIV3Parser parser = new OpenAPIV3Parser();
 		SwaggerParseResult api = parser.readLocation("src/test/resources/openapi.json", null, new ParseOptions());
 		// System.err.println(mapper.writeValueAsString(api.getOpenAPI()));
+		assertThat(api.getMessages()).isEmpty();
+	}
+
+	@Test
+	public void testValidateLinks() throws Exception {
+		OpenAPIV3Parser parser = new OpenAPIV3Parser();
+		SwaggerParseResult api = parser.readLocation("src/test/resources/links.json", null, new ParseOptions());
+		System.err.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(api.getOpenAPI()));
 		assertThat(api.getMessages()).isEmpty();
 	}
 
